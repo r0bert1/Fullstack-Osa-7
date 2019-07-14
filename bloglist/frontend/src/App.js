27e832734d5109c8a07db15aedff47ed636ad867
Robert <robert.pakkanen@helsinki.fi>
2019-07-15 00:09:53 +0300

    fixed notification

--- bloglist/frontend/src/App.js
+++ bloglist/frontend/src/App.js
@@ -75,6 +75,7 @@ const App = () => {
   const removeBlog = async (blog) => {
     const ok = window.confirm(`remove blog ${blog.title} by ${blog.author}`)
     if (ok) {
+      await blogService.remove(blog)
       setBlogs(blogs.filter(b => b.id !== blog.id))
       notify(`blog ${blog.title} by ${blog.author} removed!`)
     }
